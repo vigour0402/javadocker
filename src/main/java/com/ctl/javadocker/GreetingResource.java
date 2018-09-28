0db1ec3d4a964949947130b176aa64275b43dd4a
Kevin Bennett <kev@drule.org>
2018-09-28 22:16:57 +0000

    add external live config support

--- src/main/java/com/ctl/javadocker/GreetingResource.java
+++ src/main/java/com/ctl/javadocker/GreetingResource.java
@@ -1,25 +1,34 @@
 package com.ctl.javadocker;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.deltaspike.core.api.config.ConfigProperty;
+import org.apache.deltaspike.core.api.config.ConfigResolver;
 
 @Path("greeting")
 @ApplicationScoped
 public class GreetingResource {
 
+	private ConfigResolver.TypedResolver<String>	fooResolver	= ConfigResolver.resolve("foo").logChanges(true)
+	    .cacheFor(TimeUnit.MILLISECONDS, 10000);
+
 	@Inject
 	@ConfigProperty(name = "foo", defaultValue = "NOTHING")
-	private String fooValue;
+	private String																fooValue;
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Greeting greeting() {
-		return new Greeting(1, "hello!" + " > " + fooValue);
+		String f = fooResolver.getValue();
+		return new Greeting(1, "hello!" + " > " + fooValue + " , " + f);
 	}
 }
---
+++ src/main/java/com/ctl/javadocker/config/MyPropertyFileSource.java
@@ -0,0 +1,63 @@
+package com.ctl.javadocker.config;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.apache.deltaspike.core.api.config.Source;
+import org.apache.deltaspike.core.spi.config.ConfigSource;
+import org.apache.deltaspike.core.util.PropertyFileUtils;
+
+@ApplicationScoped
+@Source
+public class MyPropertyFileSource implements ConfigSource {
+
+	private static final String FILENAME = "etc/myapp.properties";
+
+	@Override
+	public int getOrdinal() {
+		return 150;
+	}
+
+	private Properties load() {
+		File f = new File(FILENAME);
+
+		try {
+			return PropertyFileUtils.loadProperties(f.toURI().toURL());
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("problem (re)loading property file", e);
+		}
+	}
+
+	@Override
+	public Map<String, String> getProperties() {
+		Properties p = load();
+		Map<String, String> result = new HashMap<String, String>();
+		for (String propertyName : p.stringPropertyNames()) {
+			result.put(propertyName, p.getProperty(propertyName));
+		}
+
+		return result;
+	}
+
+	@Override
+	public String getPropertyValue(String key) {
+		Properties p = load();
+		return p.getProperty(key);
+	}
+
+	@Override
+	public String getConfigName() {
+		return FILENAME;
+	}
+
+	@Override
+	public boolean isScannable() {
+		return true;
+	}
+
+}

2452ad39ac36d36f0e01043be699c26468454ac2
Kevin <kev@drule.org>
2018-10-02 01:17:10 -0600

    externalize/codeify jpa settings.

--- src/main/java/com/ctl/javadocker/data/SimpleDBEntityManagerProducer.java
+++ src/main/java/com/ctl/javadocker/data/SimpleDBEntityManagerProducer.java
@@ -6,9 +6,17 @@ import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
+import org.hibernate.tool.schema.Action;
+import org.apache.deltaspike.core.api.config.ConfigResolver;
+import org.hibernate.FlushMode;
+import org.hibernate.dialect.DerbyTenSevenDialect;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.jboss.weld.util.collections.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static org.hibernate.cfg.AvailableSettings.*;
 
+import java.util.Map;
 
 
 /**
@@ -26,8 +34,28 @@ public class SimpleDBEntityManagerProducer {
     @SimpleDB
     private EntityManager  createEM() {
     	LOG.info("Create");
+    	
+    	Map<String, Object> emProperties = ImmutableMap.<String, Object>builder()
+        .put(JPA_JDBC_DRIVER, ConfigResolver.getPropertyValue("db.driver")) 
+        .put(JPA_JDBC_URL, ConfigResolver.getPropertyValue("db.url"))
+        .put(JPA_JDBC_USER, ConfigResolver.getPropertyValue("db.user"))
+        .put(JPA_JDBC_PASSWORD, ConfigResolver.getPropertyValue("db.pass"))
+        .put(DIALECT, DerbyTenSevenDialect.class.getCanonicalName())
+        .put(HBM2DDL_AUTO, Action.CREATE)
+        .put(SHOW_SQL, false)
+        .put(FLUSH_MODE, FlushMode.AUTO)
+        .put(QUERY_STARTUP_CHECKING, false)
+        .put(GENERATE_STATISTICS, false)
+        .put(USE_REFLECTION_OPTIMIZER, false)
+        .put(USE_SECOND_LEVEL_CACHE, false)
+        .put(USE_QUERY_CACHE, false)
+        .put(USE_STRUCTURED_CACHE, false)
+        .put(STATEMENT_BATCH_SIZE, 20)
+        .put(JPA_PERSISTENCE_PROVIDER, HibernatePersistenceProvider.class.getCanonicalName())
+        .build();
+    	
         return Persistence
-                .createEntityManagerFactory("SimpleDB")
+                .createEntityManagerFactory("SimpleDB", emProperties)
                 .createEntityManager();
     }
 	
